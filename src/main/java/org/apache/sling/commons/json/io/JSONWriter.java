5320d09ccd8f44914921a853628b34ea34e1b5ef
rliechti <rliechti@adobe.com>
2024-03-14 09:59:15 +0100

    to be fully backwards compatible, change back to writer instead of appendable

--- src/main/java/org/apache/sling/commons/json/io/JSONWriter.java
+++ src/main/java/org/apache/sling/commons/json/io/JSONWriter.java
@@ -85,26 +85,17 @@ public class JSONWriter {
     /**
      * The writer that will receive the output.
      */
-    protected Appendable writer;
-
-    /**
-     * Make a fresh JSONWriter. It can be used to build one JSON text.
-     * 
-     * @param w an appendable object
-     */
-    public JSONWriter(Appendable w) {
-        this.comma = false;
-        this.mode = 'i';
-        this.stack = new JSONObject[maxdepth];
-        this.top = 0;
-        this.writer = w;
-    }
+    protected Writer writer;
 
     /**
      * Make a fresh JSONWriter. It can be used to build one JSON text.
      */
     public JSONWriter(Writer w) {
-        this((Appendable) w);
+        this.comma = false;
+        this.mode = 'i';
+        this.stack = new JSONObject[maxdepth];
+        this.top = 0;
+        this.writer = w;
     }
 
     /**
